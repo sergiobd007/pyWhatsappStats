9acaa4939915ef81c3207cbd91b102c3388c07cc
Sergio B.D <sergiobd007@gmail.com>
2019-02-01 23:51:14 +0100

    Add code

---
+++ WhatsAppStats.py
@@ -0,0 +1,56 @@
+import sqlite3
+import re
+from datetime import date, datetime
+
+
+connection = sqlite3.connect("wa.db")
+cursor = connection.cursor()
+
+def create_tables():
+
+    sql_command = "DROP TABLE IF EXISTS message"
+    cursor.execute(sql_command)
+
+    sql_command = """
+    CREATE TABLE message (
+    user VARCHAR(50),
+    message VARCHAR(10000),
+    date DATE)
+    """
+    cursor.execute(sql_command)
+
+def analyse(archivo):
+    with open(archivo) as chats:
+        numA = 0
+        numB = 0
+        lines = chats.read()
+        a = re.findall(r'^([0-9]+)\/([0-9]+)\/([0-9]+), ([0-9]+):([0-9]+) (AM|PM) - (.*?):((.|\n)*?)(?=^[0-9])', lines, re.M)
+        for elem in a:
+            month = int(elem[0])
+            day = int(elem[1])
+            year = int(elem[2])
+            hour = int(elem[3])
+            if(elem[5] == "PM"):
+                hour=(hour+12)%24
+            minute = int(elem[4])
+            name = elem[6]
+            message = elem[7].rstrip()
+#            sql_command = """ INSERT INTO message(user,message,date) VALUES(?,?,?)"""
+            date = datetime(year,month,day,hour,minute)
+            cursor.execute(sql_command, (name, message, date))
+    connection.commit()
+
+
+#create_tables()
+#analyse("chats.txt")
+sql_command = """
+SELECT strftime('%M', date) as valYear, count(message), usero
+FROM messagee
+GROUP BY valYear, usero
+HAVING user LIKE 'C%'
+"""
+
+cursor.execute(sql_command)
+results = cursor.fetchall()
+for result in results:
+    print(result[1])
